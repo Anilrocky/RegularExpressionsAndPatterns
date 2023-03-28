0fef44f60c121be415bec560dcc48031d145ea55
Anilrocky <anilkumarac96@gmail.com>
2023-03-28 20:00:55 +0530

    [Anil] Add:Check whether the entered first name is valid or not

--- RegularExpressionsAndPatterns/Program.cs
+++ RegularExpressionsAndPatterns/Program.cs
@@ -5,7 +5,24 @@ namespace RegularExpressionsAndPatterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            bool flag = true;
+            UserRegistration reg = new UserRegistration();
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Valid first name \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first name");
+                        string name = Console.ReadLine();
+                        reg.ValidateFirstName(name);                       
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }            
         }
     }
 }
---
+++ RegularExpressionsAndPatterns/UserRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionsAndPatterns
+{
+    public class UserRegistration
+    {
+        public const string FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void ValidateFirstName(string name)
+        {
+            if (Regex.IsMatch(name, FIRST_NAME))
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name");
+            }
+        }
+    }
+}
